492e279b470008d621e6ceeccef6ae1090477520
tqhien614 <tqhien614@gmail.com>
2025-09-30 13:22:01 +0700

    Update 4.php

--- 4.php
+++ 4.php
@@ -2,14 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
     <h1>This is my first PHP files</h1>
     <?php
-    $x=10;
-    $y=11;
     $x =$_GET["x"];
     $y =$_GET["y"];
     echo "x+y: " . ($x+$y) . "<br/>";
